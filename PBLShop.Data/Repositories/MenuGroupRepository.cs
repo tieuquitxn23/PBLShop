a1a7e0278947c9958e885b423aa518c8844d4863
PhamBaoLong <tieuquitxn2323@gmail.com>
2023-02-13 11:36:35 +0700

    Add more repository

---
+++ PBLShop.Data/Repositories/MenuGroupRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PBLShop.Data.Infrastructure;
+using PBLShop.Model.Models;
+
+namespace PBLShop.Data.Repositories
+{
+    public interface IMenuGroupRepository : IRepository<MenuGroup>
+    {
+    }
+
+    public class MenuGroupRepository : RepositoryBase<MenuGroup>, IMenuGroupRepository
+    {
+        public MenuGroupRepository(IDbFactory dbFactory) : base(dbFactory)
+        {
+        }
+    }
+}
---
+++ PBLShop.Data/Repositories/ProductTagRepository.cs
@@ -0,0 +1,16 @@
+﻿using PBLShop.Data.Infrastructure;
+using PBLShop.Model.Models;
+
+namespace PBLShop.Data.Repositories
+{
+    public interface IProductTagRepository : IRepository<ProductTag>
+    {
+    }
+
+    public class ProductTagRepository : RepositoryBase<ProductTag>, IProductTagRepository
+    {
+        public ProductTagRepository(IDbFactory dbFactory) : base(dbFactory)
+        {
+        }
+    }
+}
