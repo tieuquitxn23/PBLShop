cc859838385a2560f0259117893c17433e4a6407
PhamBaoLong <tieuquitxn2323@gmail.com>
2023-02-23 15:50:56 +0700

    Commit Web Api How to use Web Api

---
+++ PBLShop.Web/Api/PostCategoryController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace PBLShop.Web.Api
+{
+    public class PostCategoryController : ApiController
+    {
+        // GET: api/PostCategory
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/PostCategory/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/PostCategory
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/PostCategory/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/PostCategory/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- PBLShop.Web/App_Start/RouteConfig.cs
+++ PBLShop.Web/App_Start/RouteConfig.cs
@@ -17,6 +17,7 @@ namespace PBLShop.Web
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+
             );
         }
     }
