9dbf7f7ac78557540b0db41413427fa90feb8f12
PhamBaoLong <tieuquitxn2323@gmail.com>
2023-03-03 09:35:02 +0700

    Add web API

---
+++ PBLShop.Web/App_Start/Startup.cs
@@ -0,0 +1,35 @@
+﻿using Autofac;
+using Autofac.Integration.Mvc;
+using Microsoft.Owin;
+using Owin;
+using PBLShop.Data;
+using PBLShop.Data.Infrastructure;
+using System;
+using System.Reflection;
+using System.Threading.Tasks;
+
+[assembly: OwinStartup(typeof(PBLShop.Web.App_Start.Startup))]
+
+namespace PBLShop.Web.App_Start
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+        }
+
+        private void ConfigAutofac (IAppBuilder app)
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterControllers(Assembly.GetExecutingAssembly());
+
+            builder.RegisterType<UnitOfWork>().As<IUnitOfWork>().InstancePerRequest();
+            builder.RegisterType<DbFactory>().As<IDbFactory>().InstancePerRequest();
+
+            builder.RegisterType<PBLShopDbContext>().AsSelf().InstancePerRequest();
+
+
+        }
+    }
+}
